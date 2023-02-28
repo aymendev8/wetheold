3ff203c91e1f81f5c3dc34837e63d2c6b90efb6d
aymendev8 <aymenkadri5@gmail.com>
2023-02-28 23:52:33 +0100

    crud articles , update / delete / create done

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles;
+use App\Form\ArticleType;
 use App\Repository\ArticlesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,6 +28,58 @@ class ArticlesController extends AbstractController
 
         return $this->render('pages/articles/index.html.twig', [
             'articles' => $articles,
+        ]);
+    }
+    #[Route('/articles/modifier/{id}', name: 'articles.update')]
+    public function update(Articles $article, Request $request, EntityManagerInterface $manager): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('security.login');
+        }
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($article);
+            $manager->flush();
+            $this->addFlash('success', 'Votre article a bien été modifié');
+            return $this->redirectToRoute('articles.read');
+        }
+
+        return $this->render('pages/articles/update.html.twig', [
+            'form' => $form->createView(),
+            'article' => $article
+        ]);
+    }
+    #[Route('/articles/supprimer/{id}', name: 'articles.delete')]
+    public function delete(Articles $article, EntityManagerInterface $manager): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('security.login');
+        }
+        $manager->remove($article);
+        $manager->flush();
+        $this->addFlash('success', 'Votre article a bien été supprimé');
+        return $this->redirectToRoute('articles.read');
+    }
+
+    #[Route('/articles/ajouter', name: 'articles.create')]
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('security.login');
+        }
+        $article = new Articles();
+        $form = $this->createForm(ArticleType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($article);
+            $manager->flush();
+            $this->addFlash('success', 'Votre article a bien été ajouté');
+            return $this->redirectToRoute('articles.read');
+        }
+
+        return $this->render('pages/articles/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,21 +4,112 @@ namespace App\Form;
 
 use App\Entity\Articles;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Validator\Constraints as Assert;
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('imageName')
-            ->add('marque')
-            ->add('prix')
-            ->add('description')
-            ->add('createdAt')
-            ->add('updatedAt')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'article',
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 255,
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-2'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir un nom d\'article',
+                    ]),
+                    new Assert\Length([
+                        'min' => 2,
+                        'max' => 255
+                    ]),
+                ]
+            ])
+            ->add('imageName', FileType::class, [
+                'label' => 'Image',
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 255,
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-2'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir une image',
+                    ]),
+                    new Assert\Length([
+                        'min' => 2,
+                        'max' => 255
+                    ]),
+                ]
+            ])
+            ->add('marque', TextType::class, [
+                'label' => 'Marque',
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 255,
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-2'
+                ],
+            ])
+            ->add('prix', MoneyType::class, [
+                'label' => 'Prix',
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 255,
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-2'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir un prix',
+                    ])
+                ]
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 500,
+                ],
+                'label_attr' => [
+                    'class' => 'form-label mt-2'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir une description',
+                    ]),
+                    new Assert\Length([
+                        'min' => 2,
+                        'max' => 500
+                    ]),
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Mettre à jour',
+                'attr' => [
+                    'class' => 'btn btn-primary mt-3'
+                ]
+            ])
         ;
     }
 
